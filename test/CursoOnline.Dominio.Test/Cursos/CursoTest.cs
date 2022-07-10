62661589730978ace338623be29120f19863688c
George Duarte <contato@georgeduarte.com.br>
2022-07-10 18:00:32 -0300

    Testando dominio de Curso

---
+++ test/CursoOnline.Dominio.Test/Cursos/CursoTest.cs
@@ -0,0 +1,42 @@
+﻿using Xunit;
+
+namespace CursoOnline.Dominio.Test.Cursos
+{
+    public class CursoTest
+    {
+        [Fact]
+        public void DeveCriarCurso()
+        {
+            // Arrange
+            string nome = "Informatica Básica";
+            double cargaHoraria = 80;
+            string publicoAlvo = "Estudante";
+            decimal valor = 950;
+
+            // ACT
+            var curso = new Curso(nome, cargaHoraria, publicoAlvo, valor);
+
+            // Assert
+            Assert.Equal(nome, curso.Nome);
+            Assert.Equal(cargaHoraria, curso.CargaHoraria);
+            Assert.Equal(publicoAlvo, curso.PublicoAlvo);
+            Assert.Equal(valor, curso.Valor);
+        }
+    }
+
+    public class Curso
+    {
+        public Curso(string nome, double cargaHoraria, string publicoAlvo, decimal valor)
+        {
+            Nome = nome;
+            CargaHoraria = cargaHoraria;
+            PublicoAlvo = publicoAlvo;
+            Valor = valor;
+        }
+
+        public string Nome { get; private set; }
+        public double CargaHoraria { get; private set; }
+        public string PublicoAlvo { get; private set; }
+        public decimal Valor { get; private set; }
+    }
+}
