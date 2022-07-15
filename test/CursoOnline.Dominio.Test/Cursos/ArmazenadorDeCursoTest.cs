127ae1c02b5e97aa61744129f82e610c19f2ab9f
George Duarte <contato@georgeduarte.com.br>
2022-07-15 20:07:27 -0300

    Implementando testes com Mock

---
+++ test/CursoOnline.Dominio.Test/Cursos/ArmazenadorDeCursoTest.cs
@@ -0,0 +1,61 @@
+﻿using Xunit;
+using CursoOnline.Dominio.Cursos;
+using Moq;
+
+namespace CursoOnline.Dominio.Test.Cursos
+{
+    public class ArmazenadorDeCursoTest
+    {
+        [Fact]
+        public void DeveAdicionarCurso()
+        {
+            var cursoDto = new CursoDto
+            {
+                Nome = "Curso A",
+                Descricao = "Descrição",
+                CargaHoraria = 80,
+                PublicoAlvo = 1,
+                Valor = 951.11m
+            };
+
+            var cursoRepositorioMock = new Mock<ICursoRepositorio>();
+
+            var armazenadoreDeCurso = new ArmazenadoreDeCurso(cursoRepositorioMock.Object);
+
+            armazenadoreDeCurso.Armazenar(cursoDto);
+
+            cursoRepositorioMock.Verify(r => r.Adicionar(It.IsAny<Curso>()));
+        }
+    }
+
+    public interface ICursoRepositorio
+    {
+        void Adicionar(Curso curso);
+    }
+
+    public class ArmazenadoreDeCurso
+    {
+        private readonly ICursoRepositorio _cursoRepositorio;
+
+        public ArmazenadoreDeCurso(ICursoRepositorio cursoRepositorio)
+        {
+            _cursoRepositorio = cursoRepositorio;
+        }
+
+        public void Armazenar(CursoDto cursoDto)
+        {
+            var curso = new Curso(cursoDto.Nome, cursoDto.Descricao, cursoDto.CargaHoraria, PublicoAlvo.Estudante, cursoDto.Valor);
+
+            _cursoRepositorio.Adicionar(curso);
+        }
+    }
+
+    public class CursoDto
+    {
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public double CargaHoraria { get; set; }
+        public int PublicoAlvo { get; set; }
+        public decimal Valor { get; set; }
+    }
+}
